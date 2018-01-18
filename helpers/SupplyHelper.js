d941caaf2dcd6a039cce358c9b761262636809ff
Viktor <pulyakviktor@gmail.com>
2018-01-18 15:07:15 +0300

    Upd total Pos Amount
    Conflicts:
    	helpers/SupplyHelper.js
    	package.json

--- helpers/SupplyHelper.js
+++ helpers/SupplyHelper.js
@@ -11,18 +11,7 @@ module.exports = {
         var newCoinSinceGenesis = newHeightSinceGenesis * 1250;
         var supply = newCoinSinceGenesis + 79800000000;
         return (new BigNumber(supply));
-    },
-
-    /**
-     *
-     * @param {Number} height
-     * @return {BigNumber}
-     */
-    getPOSTotalSupplyByHeight: function (height) {
-        // Aprox to half. TODO: Calculate number of PoS blocks at this height
-        var newHeightSinceGenesis = height - 798;
-        var newCoinSinceGenesis = newHeightSinceGenesis * 1250;
-        var supply = newCoinSinceGenesis / 2;
-        return new BigNumber(supply);
     }
+
 };
+>>>>>>> f3c4410... Upd total Pos Amount
