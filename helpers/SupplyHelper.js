7b32ef7870d3695fd8a1293e399459d90b5e1b70
Peter Bushnell <bushsolo@gmail.com>
2018-06-27 06:23:28 +0000

    Total supply helper

--- helpers/SupplyHelper.js
+++ helpers/SupplyHelper.js
@@ -7,7 +7,10 @@ module.exports = {
      * @return {BigNumber}
      */
     getTotalSupplyByHeight: function (height) {
-        return (new BigNumber(100000000)).plus((height - 5000) * 4);
+        var newHeightSinceGenesis = height - 798;
+        var newCoinSinceGenesis = newHeightSinceGenesis * 1250;
+        var supply = newCoinSinceGenesis + 79800000000;
+        return (new BigNumber(supply));
     },
 
     /**
@@ -16,6 +19,10 @@ module.exports = {
      * @return {BigNumber}
      */
     getPOSTotalSupplyByHeight: function (height) {
-        return new BigNumber((height - 5000) * 4);
+        // Aprox to half. TODO: Calculate number of PoS blocks at this height
+        var newHeightSinceGenesis = height - 798;
+        var newCoinSinceGenesis = newHeightSinceGenesis * 1250;
+        var supply = newCoinSinceGenesis / 2;
+        return new BigNumber(supply);
     }
 };
